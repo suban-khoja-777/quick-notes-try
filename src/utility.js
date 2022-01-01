cb19ab74d0104f5acbbd2f56d8da75a1198997b7
suban-khoja-777 <69012061+suban-khoja-777@users.noreply.github.com>
2022-01-01 17:08:47 +0530

    Update utility.js

--- src/utility.js
+++ src/utility.js
@@ -18,4 +18,20 @@ export const normalizeContent = (content) => {
     );
 }
 
+export const debounce = (func, wait) => {
+    var timeout;
+
+    return function executedFunction() {
+        var context = this;
+        var args = arguments;
+            
+        var later = function() {
+            timeout = null;
+            func.apply(context, args);
+        };
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+};
 
